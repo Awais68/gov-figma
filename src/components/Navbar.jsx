f0f3797298a9636edfc345b70263b90bd85d5703
Mr. Awais <161300531+Awais68@users.noreply.github.com>
2024-12-07 18:29:31 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,31 +8,37 @@ import { IoCartOutline } from "react-icons/io5";
 export default function Navbar() {
 	return (
 		<header className='text-gray-600 body-font'>
-			<div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
-				<a className='flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0'>
+			<div className=' w-[90%] mx-auto  flex flex-wrap p-5 flex-col md:flex-row items-center justify-between'   >
+				<div>
+
+				<a className='flex title-font font-medium items-center text-gray-900 '>
 					<span className='ml-3 text-xl font-extrabold'>Exclusive</span>
 				</a>
-				<nav className='md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center'>
-					<Link href={"/"} className='mr-5'>
+				</div>
+				<nav className=' flex flex-wrap items-center text-base justify-center gap-5'>
+					<Link href={"/"} className=''>
 						{" "}
 						Home{" "}
 					</Link>
-					<Link href={"/contact"} className='mr-5'>
+					<Link href={"/contact"} className=''>
 						{" "}
 						Contact{" "}
 					</Link>
-					<Link href={"/about"} className='mr-5'>
+					<Link href={"/about"} className=''>
 						{" "}
 						About{" "}
 					</Link>
-					<Link href={"/singup"} className='mr-5'>
+					<Link href={"/singup"} className=''>
 						{" "}
 						Sign Up{" "}
 					</Link>
 				</nav>
+				<div className="flex justify-between items-center gap-5">
+
 				<Searchbar />
-				<FaRegHeart size={24} className='mx-5' />
+				<FaRegHeart size={24} className='' />
 				<IoCartOutline size={24} />
+				</div>
 			</div>
 		</header>
 	);
